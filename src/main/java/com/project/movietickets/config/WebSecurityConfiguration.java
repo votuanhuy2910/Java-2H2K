00f0694f635c691f9977b81e29b6ad59941b0013
stun1999 <langtupg006@gmai.com>
2021-11-19 18:50:22 +0700

    config and service

--- src/main/java/com/project/movietickets/config/WebSecurityConfiguration.java
+++ src/main/java/com/project/movietickets/config/WebSecurityConfiguration.java
@@ -1,13 +1,18 @@
 package com.project.movietickets.config;
 
+import com.project.movietickets.model.Role;
+import com.project.movietickets.service.MyUserDetailsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebSecurity
@@ -16,10 +21,12 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
 
+    private final MyUserDetailsService userDetailsService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
+                .userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder);
     }
 
@@ -28,9 +35,13 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/login").permitAll()
+                .antMatchers("/amount").permitAll()
                 .antMatchers("/").permitAll()
+                .antMatchers("/movies/**").permitAll()
                 .antMatchers("/register").permitAll()
                 .antMatchers("/files/**").permitAll()
+                .antMatchers("/restore-password", "/confirm-password").permitAll()
+                .antMatchers("/manage/**").hasRole("EMPLOYEE")
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().permitAll()
                 .and()
